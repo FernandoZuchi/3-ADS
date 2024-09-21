905c87b649e2cbeafccfb153c7de1a368287d631
FernandoZuchi <nandozuchi07@gmail.com>
2024-09-20 21:45:49 -0300

    PFS

--- PFS/crud-cliente/src/App.js
+++ PFS/crud-cliente/src/App.js
@@ -1,13 +1,54 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 
-const App = () => {
-  useEffect(() => {
-    axios.get('http://localhost:3005/feeds')
+function App(){
+  const [feeds, setFeeds] = useState(null);
+  const [novoFeed, setNovoFeed] = useState({
+    id: "",
+    usuario: "",
+    tempo: ""
   })
 
+  const carregarDados = () => {
+    axios.get("http://localhost:3005/feeds").then(
+      result => setFeeds(result.data)
+    );
+  };
+
+  const excluir = (id) => {
+    axios.delete(`hhttp://localhost:3005/feeds/${id}`).then(
+      result => console.log(result)
+    )
+  }
+
+  useEffect(() => {
+    carregarDados();
+  }, []);
+
+  if(!feeds) {
+    return <div>Carregando</div>
+  }
+
   return (
-    <div>App</div>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Usuário</th>
+            <th>Tempo</th>
+          </tr>
+        </thead>
+        <tbody>
+          {feeds.map(x => {
+            <tr>
+              <td>{x.usuario}</td>
+              <td>{x.tempo}</td>
+              <td><button onClick={() => excluir(x.id)}>Excluir</button></td>
+            </tr>
+          })}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
