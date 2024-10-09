67ec4163a824b6b53933f0f0ee09fd7a191928c4
FernandoZuchi <nandozuchi07@gmail.com>
2024-10-08 21:49:29 -0300

    Criando o primeiro projeto com ASP.NET

---
+++ PFS/AtletaApi/Endpoints/AtletaEndpoints.cs
@@ -0,0 +1,11 @@
+﻿namespace AtletaApi;
+
+public static class AtletaEndpoints
+{
+    internal static void AdicionarAtletaEndpoints(this WebApplication app)
+    {
+        app.MapGet("/", () => 
+        "Olá mundo!"
+      );
+    }
+}
