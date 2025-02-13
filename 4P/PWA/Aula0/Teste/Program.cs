7d0f4190734b2f1314b544b30d22cba445d8902c
FernandoZuchi <nandozuchi07@gmail.com>
2025-02-13 18:02:12 -0300

    ✨ feat + ♻️  refactor: Organização de pastas 3º e 4º período

---
+++ 4P/PWA/Aula0/Teste/Program.cs
@@ -0,0 +1,7 @@
+﻿using MinhaDLL;
+
+var nome = "fernando";
+var disciplina = "PWA";
+
+// MeuConsole.EscrevaLinha("{0}, seja bem-vindo à disciplina {1}!", nome, disciplina);
+"{0}, seja bem-vindo à disciplina {1}!".EscrevaLinha(nome.InicialMaiscula(), disciplina);
